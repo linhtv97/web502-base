92cf4361f8696ab9be2cea7a9a2fdb5f4e3c89af
linh.cntt097@gmail.com <linh.cntt097@gmail.com>
2024-04-09 20:56:35 +0700

    Revert "Tutor ts react"
    This reverts commit f7f2350ddd8c5bbbd76bdbba470fe09d98ea443d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import PostsPage from "./pages/PostsPage";
-import PostAdd from "./pages/PostAdd";
-import PostEdit from "./pages/PostEdit";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/posts",
-    element: <PostsPage />,
-  },
-  {
-    path: "/posts/add",
-    element: <PostAdd />,
-  },
-  {
-    path: "/posts/edit/:id",
-    element: <PostEdit />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-]);
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <h1>Chúc các bạn thi tốt!</h1>
     </>
   );
 }
